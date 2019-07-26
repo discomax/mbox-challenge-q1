506aba6ef18604854f1cdc5e745e4f6d70a5e6c9
discomax <mpatterson73@gmail.com>
2019-07-26 18:00:20 -0400

    added unicode message handling

--- mbox_challenge.py
+++ mbox_challenge.py
@@ -1,10 +1,10 @@
 
 import mailbox as mb
-from email import utils, message
+from email import message, charset, policy
 
 # Store the file path associated with the file (note the backslash may be OS specific)
-file = 'mbox.full'
-mbox = mb.mbox(file)
+imput_file = 'data/mbox.full'
+mbox = mb.mbox(imput_file)
 mbox.lock()
 
 """def reverse_lines(string):
@@ -21,28 +21,27 @@ def reverse_payload(mail_message):
     
         #print(mbox_msg.__dict__)
         for sub_msg in payload:              
-            div = ""
-            print(div)
+            #div = ""
+            #print(div)
+            #print(type(sub_msg))
             #print(sub_msg.__dict__)
             #print(sub_msg.get_content_type())
             #print(subMsg)
             #lines = subMsg.splitlines(_m
             #print(vars(sub_msg))
             reverse_payload(sub_msg)
-            div = '------------------------'
+            #div = '------------------------'
     else:
     # create new message with reversed 
-        #print(mbox_msg.__dict__)
+        print(mail_message.__dict__)
         #orig_content = mbox_msg.get_payload()
         print(payload)
-        print(type(payload))
+        #print(type(payload))
         lines = payload.splitlines()
         rev_lines = lines[::-1]
-        print(type(rev_lines))
-        #print(orig_content)
-        print(rev_lines)
         rev_payload = "\n".join(rev_lines)
         print(rev_payload)
+        return rev_payload
 
 
 def revise_msg(mbox_msg):
@@ -51,13 +50,32 @@ def revise_msg(mbox_msg):
     # of the message body/payload in reverse order.
     # returns the new Mbox message.
     msg = mbox_msg
-    payload = mbox_msg.get_payload()
-    reverse_payload(msg)
+    msg_content = reverse_payload(msg)
+    utf8msg = msg_content.encode("utf-8")
+    print(utf8msg)
+    #print(msg_content)
+    msg.set_payload(msg_content)
+    return msg
+    #reverse_payload(msg)
     
-    
+out_file = 'data/revised_mbox.full'
+
 #for message in mbox:
-revise_msg(mbox[0])
-mbox.unlock()
+
+try:
+    revised_mbox = mb.mbox(out_file)
+    #for message in mbox:
+    message = mbox[9]
+    #print(message.get_payload())
+    #print('\n\n')
+    new_msg = revise_msg(message)
+    #print(vars(new_msg))
+    #revised_mbox.add(new_msg)
+    #print('success')
+finally:
+    mbox.unlock()
+    revised_mbox.clear()
+    mbox.close()
 #if __name__ == '__main__':
     
 #print(repr(msg_body))
