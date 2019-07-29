7a28ffcb1889fa13b29807c926abcd4ddaae7867
discomax <mpatterson73@gmail.com>
2019-07-29 15:42:48 -0400

    added multipart message handling

--- mbox_challenge.py
+++ mbox_challenge.py
@@ -7,78 +7,92 @@ imput_file = 'data/mbox.full'
 mbox = mb.mbox(imput_file)
 mbox.lock()
 
-"""def reverse_lines(string):
-    # 
-    lines = string.splitlines()
-    #print(lines)
-    rev_lines = lines[::-1]
-    #print(rev_lines)
-    return rev_lines"""
-    
+
+
+
+
+    #returns a message list
+
 def reverse_payload(mail_message):
-    payload = mail_message.get_payload()
-    if mail_message.is_multipart():
+    msg = mail_message
+    payload = msg.get_payload()
     
-        #print(mbox_msg.__dict__)
-        for sub_msg in payload:              
-            #div = ""
-            #print(div)
-            #print(type(sub_msg))
-            #print(sub_msg.__dict__)
-            #print(sub_msg.get_content_type())
-            #print(subMsg)
-            #lines = subMsg.splitlines(_m
-            #print(vars(sub_msg))
-            reverse_payload(sub_msg)
-            #div = '------------------------'
-    else:
-    # create new message with reversed 
-        #print(mail_message.__dict__)
-        #orig_content = mbox_msg.get_payload()
-        #print(payload)
-        #print(type(payload))
-        lines = payload.splitlines()
-        rev_lines = lines[::-1]
-        rev_payload = "\n".join(rev_lines)
-        #print(rev_payload)
-        return rev_payload
+    lines = payload.splitlines()
+    rev_lines = lines[::-1]
+    rev_payload = "\n".join(rev_lines)
+    print(rev_payload)
+    content_type = msg.get('Content-Type')
+    encoding = msg.get('Content-Transfer-Encoding')
+    if 'Content-Transfer-Encoding' in msg:
+        if '8bit' in encoding.lower():
+            rev_payload = rev_payload.encode("utf-8")
+        else:
+            # TODO: handle other encodings (i.e. base64, Binary, x-token)
+            pass
+    return rev_payload
 
 
+def process_multipart(mail_message):
+    msg = mail_message
+    payload = msg.get_payload()
+    print(len(payload))
+    new_payload = []
+    print(type(payload))
+    '''for sub_msg in payload:
+        if sub_msg.is_multipart():
+            process_multipart(sub_msg)
+        else:
+            content = reverse_payload(msg)    
+            print(content)
+            msg.set_payload(content)
 
-def revise_msg(mbox_msg):
+        if 'related' in msg.get('Content-Type').lower():
+            sub_msg = sub_msg.make_related()
+        elif 'alternative' in msg.get('Content-Type').lower():
+            sub_msg = sub_msg.make_alternative()
+        else:
+            #sub_msg = sub_msg.make_mixed()
+            sub_msg = '''
+    sub_msg = payload[0]
+    sub_msg.set_payload(reverse_payload(sub_msg))
+    msg.set_payload(new_payload)
+    msg.attach(sub_msg)
+    print(vars(msg))
+    print(len(msg.get_payload()))
+    return msg
+
+            
+
+def revise_msg(mail_message):
     # takes an Mbox mail meassage as the argument
     # creates a new Mbox meassage instance with the lines 
     # of the message body/payload in reverse order.
     # returns the new Mbox message.
-    msg = mbox_msg
-    content_type = msg.get('Content-Type')
-    encoding = msg.get('Content-Transfer-Encoding')
+    msg = mail_message
     
-    msg_content = reverse_payload(msg)
-    if 'Content-Transfer-Encoding' in msg:
-        if '8bit' in encoding.lower():
-            msg_content = msg_content.encode("utf-8")
-        else:
-            # TODO: handle other encodings (i.e. base64, Binary, x-token)
-            pass
-
-    #print(utf8msg)
-    print(msg_content)
-    msg.set_payload(msg_content)
+    
+    if msg.is_multipart():
+        msg = process_multipart(msg)
+        pass
+    else:
+        msg_content = reverse_payload(msg)    
+        print(msg_content)
+        msg.set_payload(msg_content)
     return msg
     #reverse_payload(msg)
     
-out_file = 'data/revised_mbox.full'
+
 
 #for message in mbox:
 
 try:
+    out_file = 'data/revised_mbox.full'
     revised_mbox = mb.mbox(out_file)
-    for message in mbox:
-        #message = mbox[0]
-        new_msg = revise_msg(message)
+    for Message in mbox:
+        #Message = mbox[10]
+        NewMessage = revise_msg(Message)
         #print(vars(new_msg))
-        revised_mbox.add(new_msg)
+        revised_mbox.add(NewMessage)
         #print('success')
 finally:
     mbox.unlock()
