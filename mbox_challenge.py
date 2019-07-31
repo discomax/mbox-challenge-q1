cdb0479dcf3ffe0b08e0988ee0a386f3fd44ad2c
discomax <mpatterson73@gmail.com>
2019-07-31 00:19:32 -0400

    added exception handling

--- mbox_challenge.py
+++ mbox_challenge.py
@@ -17,28 +17,22 @@ def pop_signature(line_list, delim='-- '):
     if delim in line_list:
         new_list = line_list[:line_list.index(delim)]
         sig_list = line_list[line_list.index(delim):]
-        #print(line_list)
-        #print(new_list)
-        #print(sig_list)
-        #print(line_list.index(delim))
+
         return new_list, sig_list
     else:
         sig_list = []
+
         return line_list, sig_list
+
 
 def reverse_payload(mail_message):
     msg = mail_message
     payload = msg.get_payload()
     
     lines, signature = pop_signature(payload.splitlines())
-    #print(lines, signature)
     reversed_lines = lines[::-1]
     reversed_lines.extend(signature)
-    #reversed_lines = reversed_lines.extend(signature)
-    #reversed_lines = reversed_lines.append(signature)
-    print(reversed_lines)
     reversed_payload = "\n".join(reversed_lines)
-    print(reversed_payload)
     content_type = msg.get('Content-Type')
     encoding = msg.get('Content-Transfer-Encoding')
     if 'Content-Transfer-Encoding' in msg:
@@ -51,6 +45,7 @@ def reverse_payload(mail_message):
     return reversed_payload
 
 
+
 def process_multipart(msg, payload=[]):
     """
     Reverse lines of multipart payload's string part.
@@ -60,7 +55,6 @@ def process_multipart(msg, payload=[]):
     payload -- list of messages (default [])
     """
     content = msg.get_payload()
-    #print(len(payload))
     new_payload = []
     sub_msg = content[0]
     sub_msg.set_payload(reverse_payload(sub_msg))
@@ -69,7 +63,8 @@ def process_multipart(msg, payload=[]):
     
     return msg
 
-            
+
+
 def revise_msg(mail_message):
     # takes an Mbox mail meassage as the argument
     # creates a new Mbox meassage instance with the lines 
@@ -78,34 +73,32 @@ def revise_msg(mail_message):
     msg = mail_message
     if msg.is_multipart():
         msg = process_multipart(msg)
-        #pass
     else:
         msg_content = reverse_payload(msg)    
-        #print(msg_content)
         msg.set_payload(msg_content)
     return msg
 
 
 
-def mbox_challenge():
-    imput_file = 'data/mbox.full'
-    mbox = mb.mbox(imput_file)
-    mbox.lock()
-    try:
-        out_file = 'data/revised_mbox.full'
-        revised_mbox = mb.mbox(out_file)
-        for Message in mbox:
-            #Message = mbox[4]
-            NewMessage = revise_msg(Message)
-            #print(NewMessage)
-            revised_mbox.add(NewMessage)
-        #print('success')
-    finally:
-        mbox.unlock()
-        #revised_mbox.clear()
-        mbox.close()
+def mbox_challenge(in_file, out_file):
+    with open(in_file):
+        try:
+            mbox = mb.mbox(in_file)
+            mbox.lock()
+            revised_mbox = mb.mbox(out_file)
+            for Message in mbox:
+                #Message = mbox[4]
+                NewMessage = revise_msg(Message)
+                revised_mbox.add(NewMessage)
+        except (mb.NoSuchMailboxError, mb.FormatError) as e:
+            print('Error Occurerd: ', e)
+        finally:
+            revised_mbox.close()
+            mbox.unlock()
+
 
 
 if __name__ == "__main__":
-    mbox_challenge()
-#print(repr(msg_body))
+    mbox_in = 'date/mbox.full'
+    mbox_out = 'data/revised_mbox.full'
+    mbox_challenge(mbox_in, mbox_out)
