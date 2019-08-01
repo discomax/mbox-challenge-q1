66df63dd32bbf322030e27395edadde7f67093e8
discomax <mpatterson73@gmail.com>
2019-07-31 22:24:55 -0400

    added script docstring

--- mbox_challenge.py
+++ mbox_challenge.py
@@ -1,11 +1,21 @@
+'''
+Mbox Challenge
+
+This script reads an email mbox file filled with singlepart
+and multipart/mixed email messages with a content  type of 
+text/plain. It takes the mbox file and genrates anoutner mbox
+file with all the mbox files in the same order, but with the
+lines of each individual message bady printed in reverse order.
+
+This tool accepts text (.txt) files as well as mabox (.full) files. 
+
+This script requires `mailbox` be installed within the  Python
+environment you are running the script in.
+'''
 
 import mailbox as mb
 from email import message
 
-# Store the file path associated with the file (backslash may be OS specific)
-#imput_file = 'data/mbox.full'
-#mbox = mb.mbox(imput_file)
-#mbox.lock()
 
 def pop_signature(lines, delim='-- '):
     '''Splits content into body and signature
